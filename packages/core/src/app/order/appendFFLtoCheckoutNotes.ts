b7a8ac4105e9ac67a167675a6297080c66dac165
Anton Santiago <antons@razoyo.com>
2025-11-19 22:08:30 +0800

    FRM-305 adds FFL certificate link in order comments

--- packages/core/src/app/order/appendFFLtoCheckoutNotes.ts
+++ packages/core/src/app/order/appendFFLtoCheckoutNotes.ts
@@ -3,6 +3,7 @@ export default async function appendFFLtoCheckoutNotes(
   checkout,
   updateCheckout,
   selectedFFL,
+  storeHash
 ): Promise<CheckoutSelectors> {
   // Appends FFL information to the checkout order comments in the following format:
   // Format: <existing message>|FFL#<license>|Expiration:<date>|EZcheck:<url>
@@ -20,18 +21,43 @@ export default async function appendFFLtoCheckoutNotes(
     L: '11',
     M: '12',
   };
-  const expiryMonth = months[selectedFFL.slice(13, 14)];
-  const expiryYear = `202${selectedFFL.slice(12, 13)}`;
+  const expiryMonth = months[selectedFFL.fflID.slice(13, 14)];
+  const expiryYear = `202${selectedFFL.fflID.slice(12, 13)}`;
 
   // Parse FFL number components for ATF link
   // Split by dashes and get relevant parts
-  const fflParts = selectedFFL.split('-');
+  const fflParts = selectedFFL.fflID.split('-');
   const licsRegn = fflParts[0]; // First part (6)
   const licsDis = fflParts[1]; // Second part (04)
   const licsSeq = fflParts[5]; // Last part (03791)
   const atfLink = `https://fflezcheck.atf.gov/FFLEzCheck/fflSearch?licsRegn=${licsRegn}&licsDis=${licsDis}&licsSeq=${licsSeq}`;
+  const certificateURL = '';
 
-  const message = `${checkout.customerMessage}|FFL#${selectedFFL}|Expiration:${expiryMonth}/01/${expiryYear}|EZcheck:${atfLink}`;
+  await fetch(`https://${process.env.HOST}/store-front/api/${storeHash}/dealers/${selectedFFL.id}/certificate`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => {
+      if (res.status === 404) {
+        return null;
+      } else{
+        return res.json();
+      }
+    })
+    .then(data => {
+      if (data === null) {
+        console.log(`No FFL certificate found for ${selectedFFL.fflID}`)
+      } else {
+        certificateURL = `|Certificate:${data.url}`;
+      }
+    })
+    .catch(err => {
+      console.log('Certificate fetch failed:', err);
+    });
+
+  const message = `${checkout.customerMessage}|FFL#${selectedFFL.fflID}|Expiration:${expiryMonth}/01/${expiryYear}|EZcheck:${atfLink}${certificateURL}`;
 
   await updateCheckout({ customerMessage: message });
 }
